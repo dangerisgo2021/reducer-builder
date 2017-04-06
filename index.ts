41998b0ed53a382d3746c65c9dc97b8ff7286096
Clark <clark_luskin@yahoo.com>
2017-04-06 15:22:37 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,33 +1,27 @@
 interface iAction {
-  type: any
+    type: any
 }
 
-export default class ReduxReducerBuilder<Action extends iAction, ActionType, iState> {
-  private actionTypeToReducerMap: Map<ActionType, (state: iState, action: Action) => iState>;
-  private initialState? : iState;
-  constructor() {
-    this.initalState = {};
-    this.actionTypeToReducerMap = new Map<ActionType, (state: iState, action: Action) => iState>();
-  }
+export default class ReduxReducerBuilder<Action extends iAction, ActionType, State> {
+    initialState: State;
+    actionTypeToReducerMap: Map<ActionType,(state:State,action: Action)=>State> = new Map();
 
-  setInitialState(initialState: iState) : ReduxReducerBuilder<Action, ActionType, iState> {
-    this.initialState = initialState;
-    return this;
-  }
+    setInitialState(initialState: State) : ReduxReducerBuilder<Action, ActionType, State> {
+        this.initialState = initialState;
+        return this;
+    }
 
-  addReducer(actionType: ActionType,
-             reducer: (state: iState, action: Action) => iState)
-             : ReduxReducerBuilder<Action, ActionType, iState> {
-    this.actionTypeToReducerMap.set(actionType, reducer);
-    return this;
-  };
-
-  build(): (state: iState, action: Action) => iState {
-    const self = this;
-    return function(state = self.initialState, action: Action) {
-      const reducer = self.actionTypeToReducerMap.get(action.type);
-
-      return reducer ? reducer(state, action) : state;
+    addReducer(actionType: ActionType, reducer: (state: State, action: Action) => State)
+               : ReduxReducerBuilder<Action, ActionType, State> {
+        this.actionTypeToReducerMap.set(actionType, reducer);
+        return this;
     };
-  }
+
+    build(): (state: State, action: Action) => State {
+        const self = this;
+        return function(state = self.initialState, action: Action) {
+            const reducer = self.actionTypeToReducerMap.get(action.type);
+            return reducer ? reducer(state, action) : state;
+        };
+    }
 }
