621486d2aa6d629f0a362e9f7ee43a8db5c1472f
Clark <clark_luskin@yahoo.com>
2017-11-19 21:45:33 -0500

    Reset
    not using typescript anymore
    
    still es6

--- index.ts
+++ index.ts
@@ -1,27 +1,64 @@
-interface iAction {
-    type: any
-}
+import { combineReducers } from 'redux';
+import { isEmpty, keys } from 'lodash'
 
-export default class ReduxReducerBuilder<Action extends iAction, ActionType, State> {
-    initialState: State;
-    actionTypeToReducerMap: Map<ActionType,(state:State,action: Action)=>State> = new Map();
+export class ReduxReducerBuilder {
 
-    setInitialState(initialState: State) : ReduxReducerBuilder<Action, ActionType, State> {
-        this.initialState = initialState;
-        return this;
-    }
+	constructor() {
+		this.initialState = {};
+		this.actionTypeToReducer = {};
+		this.nameToChild = {};
+	}
 
-    addReducer(actionType: ActionType, reducer: (state: State, action: Action) => State)
-               : ReduxReducerBuilder<Action, ActionType, State> {
-        this.actionTypeToReducerMap.set(actionType, reducer);
-        return this;
-    };
+	setInitialState(initialState) {
+		this.initialState = initialState;
+		return this;
+	}
 
-    build(): (state: State, action: Action) => State {
-        const self = this;
-        return function(state = self.initialState, action: Action) {
-            const reducer = self.actionTypeToReducerMap.get(action.type);
-            return reducer ? reducer(state, action) : state;
-        };
-    }
+	addReducer(actionType, reducer) {
+		this.actionTypeToReducer[actionType] = reducer;
+		return this;
+	};
+
+	combine(name, child) {
+		this.nameToChild[name] = child;
+		return this;
+	}
+
+	build() {
+
+		const self = this;
+
+
+		if(isEmpty(self.nameToChild)) {
+
+			return (state = self.initialState, action) => {
+				const reducer = self.actionTypeToReducer[action.type];
+				return reducer ? reducer(state, action) : state
+			}
+
+		} else {
+
+			return (state = self.initialState, action) => {
+
+				const parentReducer = self.actionTypeToReducer[action.type];
+
+				const applyChildren = (nextState, name) => {
+					const reducer = self.nameToChild[name];
+					nextState[name] = reducer(state[name], action);
+					return nextState;
+				};
+
+				const children = _.keys(self.nameToChild)
+					.reduce(applyChildren, {});
+
+				return {
+					...(parentReducer ? parentReducer(state, action) : state),
+					...children
+				};
+
+			}
+		}
+
+	}
+
 }
